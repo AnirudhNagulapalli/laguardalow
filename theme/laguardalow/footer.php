45084924a6d7bad84eb615e310243300fdf70e99
Anirudh Nagulapalli <anirudh.nagulapalli1@marist.edu>
2018-05-11 17:19:53 -0400

    Added theme files

---
+++ theme/laguardalow/footer.php
@@ -0,0 +1,88 @@
+    </div><!-- /.container -->
+  </div>
+
+    <footer class="site-footer">
+      <div class="row-fluid">
+        <div class="col-md-8">
+          <div class="col-md-2">
+            <ul id="footer_projects">
+              <li><strong>PROJECTS</strong></li>
+              </br>
+              <li>ALL PROJECTS</li>
+              <li>CURRENT</li>
+              <li>PLANNING</li>
+              <li>MIXED USE</li>
+              <li>RETAIL</li>
+              <li>COMMERCIAL</li>
+              <li>TRANSIT</li>
+              <li>PUBLIC</li>
+              <li>ARCHIVE</li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+            <ul id="footer_expertise">
+              <li><strong>EXPERTISE</strong></li>
+              </br>
+              <li>MASTER PLANNING</li>
+              <li>MIXED USE</li>
+              <li>RETAIL DESIGN</li>
+              <li>SUSTAINABILITY</li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+            <ul id="footer_firm">
+              <li><strong>FIRM</strong></li>
+              </br>
+              <li>OVERVIEW</li>
+              <li>LEADERSHIP</li>
+              <li>VIDEOS</li>
+              <li>AWARDS</li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+            <ul id="footer_news">
+              <li><strong>NEWS</strong></li>
+              </br>
+              <li>EVENTS</li>
+              <li>CONSTRUCTION</li>
+              <li>PUBLICATIONS</li>
+              <li>AWARDS</li>
+              <li>NEWSLETTERS</li>
+            </ul>
+          </div>
+          <div class="col-md-2">
+            <ul id="footer_contact">
+              <li><strong>CONTACT</strong></li>
+              </br>
+              <li>COMMISIONS</li>
+              <li>PRESS</li>
+              <li>GENERAL</li>
+              <li>CAREERS</li>
+              <li>OFFICE LOCATIONS</li>
+            </ul>
+          </div>
+          <div class="col-md-2"></div>
+        </div>
+        <div class="col-md-4">
+          <ul id=credits>
+            <li><strong>CREDITS</strong></li>
+            </br>
+            <li>Website Design</li>
+            <li>Pisactello Design Centre</li>
+            </br>
+            <li>Photography</li>
+            <li>Placeholder Name</li>
+            </br>
+            <li>Video</li>
+            <li>Yo.Yuu Creative</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ theme/laguardalow/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>laguardia.low</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Latest compiled and minified JavaScript -->
+
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <header class="site-header">
+      <div class="row-fluid">
+        <div class="col-md-3">
+          <h1 class="site-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><!-- site title --><?php echo get_bloginfo( 'name' ); ?></a></h1>
+        </div>
+        <div class="col-md-3">
+          <!-- Search bar -->
+        </div>
+        <div class="col-md-4">
+          <nav class="blog-nav">
+            <!-- <a class="blog-nav-item active" href="#">Home</a> -->
+            <?php wp_nav_menu(); ?>
+          </nav>
+        </div>
+        <div class="col-md-2">
+          <!-- English/Chinese -->
+        </div>
+      </div>
+        <!-- <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <?php wp_nav_menu(); ?>
+        </nav> -->
+      </div>
+    </header>
+
+    <div class="row-fluid" style="background-color: #F0F0F0">
+      <div class="container">
+      </br></br>
+      
