863d48653455ed0c3390b826123cc02c48ee48b1
Anirudh Nagulapalli <anirudh.nagulapalli1@marist.edu>
2018-05-18 12:54:26 -0400

    Home page done

---
+++ theme/laguardalow/content.php
@@ -0,0 +1,11 @@
+<div class="col-sm-8 blog-main">
+	<div class="row-fluid">
+	  <div class="container" style="background-color: #A9A9A9; height: 100%">
+	      
+
+
+
+		</div><!-- /.container -->
+	</div>
+</div><!-- /.blog-main -->
+
--- theme/laguardalow/header.php
+++ theme/laguardalow/header.php
@@ -1,61 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>laguardia.low</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Latest compiled and minified JavaScript -->
-
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-    <!-- Custom styles for this template -->
-    <link href="<?php echo get_bloginfo('template_directory'); ?>/style.css" rel="stylesheet">
-
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
-
-    <header class="site-header">
-      <div class="row-fluid">
-        <div class="col-md-3">
-          <h1 class="site-title"><a href="<?php echo get_bloginfo( 'wpurl' );?>"><!-- site title --><?php echo get_bloginfo( 'name' ); ?></a></h1>
-        </div>
-        <div class="col-md-3">
-          <!-- Search bar -->
-        </div>
-        <div class="col-md-4">
-          <nav class="blog-nav">
-            <!-- <a class="blog-nav-item active" href="#">Home</a> -->
-            <?php wp_nav_menu(); ?>
-          </nav>
-        </div>
-        <div class="col-md-2">
-          <!-- English/Chinese -->
-        </div>
+<header class="site-header">
+  <div class="band row-fluid">
+    <div class="logo col-md-3">
+      <a href="http://localhost:8888/wordpress/">
+        <!-- <img class ="logo" alt="LaguardaLow"> -->
+      </a>
+    </div>
+    <div class="col-md-3">
+      <!-- Search bar -->
+      <div class="search-container">
+        <form action="/action_page.php">
+          <input id="input-area" type="text" placeholder="" name="search">
+          <button class="search" type="submit"></button>
+          <!-- <div id="input-area-slider"></div> -->
+        </form>
       </div>
-        <!-- <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <?php wp_nav_menu(); ?>
-        </nav> -->
+    </div>
+    <div class="col-md-4">
+      <nav class="site-nav">
+        <!-- <a class="site-nav-item active" href="#">Home</a> -->
+        <?php wp_nav_menu(); ?>
+      </nav>
+    </div>
+    <div class="col-md-2">
+      <!-- English/Chinese -->
+      <div class="lang-toggle">
+        <!-- <p><span style="color: #ED2939">English</span> &nbsp; 中文</p> -->
+        <div id="lang-en">ENGLISH</div>
+        <div id="lang-zh">中文</div>
       </div>
-    </header>
+    </div>
+  </div>
+  <div class="row-fluid slider-wrapper theme-laguardalowSlider">
+    <div id="slider" class="nivoSlider">
+      <img src="http://localhost:8888/wordpress/wp-content/uploads/2018/05/LLA_Homepage1.jpg" alt=""  title="#htmlcaption"/>
+      <img src="http://localhost:8888/wordpress/wp-content/uploads/2018/05/LLA_Homepage2.jpg" alt=""  title="#htmlcaption"/>
+      <img src="http://localhost:8888/wordpress/wp-content/uploads/2018/05/LLA_Homepage3.jpg" alt=""  title="#htmlcaption"/>
+      <img src="http://localhost:8888/wordpress/wp-content/uploads/2018/05/LLA_Homepage4.jpg" alt=""  title="#htmlcaption"/>
+    </div>
+    <div id="htmlcaption" class="nivo-html-caption">
+      <p>Laguarda.Low is a global</br>architectural practice focused on</br>creating exciting experiments.</p> 
+    </div>
+  </div>
+</header>
 
-    <div class="row-fluid" style="background-color: #F0F0F0">
-      <div class="container">
-      </br></br>
-      
+    
